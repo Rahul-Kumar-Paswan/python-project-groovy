e65cfa6ba629db4fb6686cb99244d81bd29d6431
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-07-05 14:35:32 +0530

    adding all files

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ COPY . /app
 RUN pip install -r requirements.txt
 EXPOSE  3000
 CMD ["python","main.py"]
+
--- script.groovy
+++ script.groovy
@@ -13,8 +13,13 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
+    def dockerCmd = 'docker run -d --name rahul-project -p 3000:3000 rahulkumarpaswan/my-python-project:1.2'
+    sshagent(['ec2-user-Rahul']) {
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@13.232.90.226 ${dockerCmd}"
+    }
 } 
 
 return this
 
 //
+
